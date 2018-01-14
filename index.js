eebf94e689c5b44cd556a36132c9a18915a3457f
Mauricio Labarca <mauriciolabarca4tw@gmail.com>
2018-01-14 11:29:34 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const express = require('express');
+
+const dishRouter = require('./routes/dishrouter');
+
+
+const app = express();
+app.use('/dishes', dishRouter);
+
+app.use((req, res, next) => {
+  console.log(req.headers);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+
+});
+
+const server = http.createServer(app);
+const hostname = 'localhost';
+const port = 3000;
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
